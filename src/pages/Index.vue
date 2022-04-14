5f05796fa7b5a2af07e800a9401d83ffb8306b32
Aniket-Webol <aniket@webol.co.uk>
2022-04-15 03:43:09 +0530

    added phone layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,56 +1,94 @@
 <template>
-  <q-page style="margin: 40px;">
+  <q-page :class="{ 'phone-margin phone-background': isPhone, 'web-margin': !isPhone }">
     <!-- page header -->
-    <div class="row">
-      <div class="col-12" style="font-size: 14px; margin-bottom: 18px;">Available balance</div>
-      <div class="col-auto" style="padding: 2px 13px 2px 13px; background-color: #01D167; color: #FFFFFF; display: inline-block; border-radius: 4px; align-self: center;">S$</div>
-      <div class="col-6" style="font-size: 26px; display: inline-block; font-weight: 700; padding: 2px 13px 2px 13px">3,000</div>
-      <div class="col text-right">
-        <q-btn no-caps style="background-color: #325BAF;" @click="addCard">
-          <q-img left src="/img/box_1.svg" style="width: 16px; height: 16px; : #fff" />
-          <span style="margin-left: 10px; color: #fff">New Card</span>
-        </q-btn>
+    <template v-if="!isPhone">
+      <div class="row">
+        <div class="col-12" style="font-size: 14px; margin-bottom: 18px;">Available balance</div>
+        <div class="col-auto" style="padding: 2px 13px 2px 13px; background-color: #01D167; color: #FFFFFF; display: inline-block; border-radius: 4px; align-self: center;">S$</div>
+        <div class="col-6" style="font-size: 26px; display: inline-block; font-weight: 700; padding: 2px 13px 2px 13px">3,000</div>
+        <div class="col text-right">
+          <q-btn no-caps style="background-color: #325BAF;" @click="addCard">
+            <q-img left src="/img/box_1.svg" style="width: 16px; height: 16px; : #fff" />
+            <span style="margin-left: 10px; color: #fff">New Card</span>
+          </q-btn>
+        </div>
       </div>
-    </div>
+    </template>
+
+    <template v-if="isPhone">
+      <div class="row">
+        <div class="col-12" style="font-size: 14px; margin-bottom: 18px;">
+          <div class="row" style="line-height: 3em;">
+            <div class="col">Available balance</div>
+            <div class="col text-right">
+              <q-img src="/img/Logo.svg" style="width: 25px; height: 25px; padding: 0px; 10px 10px 0px;" />
+            </div>
+          </div>
+        </div>
+        <div class="col-auto" style="padding: 2px 13px 2px 13px; background-color: #01D167; color: #FFFFFF; display: inline-block; border-radius: 4px; align-self: center;">S$</div>
+        <div class="col-6" style="font-size: 26px; display: inline-block; font-weight: 700; padding: 2px 13px 2px 13px">3,000</div>
+        <div class="col text-right">
+          <q-btn no-caps style="background-color: #325BAF;" @click="addCard">
+            <q-img left src="/img/box_1.svg" style="width: 16px; height: 16px; : #fff" />
+            <span style="margin-left: 10px; color: #fff">New Card</span>
+          </q-btn>
+        </div>
+      </div>
+    </template>
 
     <!-- tabs -->
-    <div class="row q-mt-md">
-      <q-tabs
-        v-model="tab"
-        dense
-        indicator-color="accent"
-        align="left"
-        class="col-12"
-      >
-        <q-tab :ripple="false" no-caps name="my_debit_cards" label="My debit cards" />
-        <q-tab :ripple="false" disable no-caps name="all_company_cards" label="All company cards" />
-      </q-tabs>
+    <template v-if="!isPhone">
+      <div class="row q-mt-md">
+        <q-tabs
+          v-model="tab"
+          dense
+          indicator-color="accent"
+          align="left"
+          class="col-12"
+        >
+          <q-tab :ripple="false" no-caps name="my_debit_cards" label="My debit cards" />
+          <q-tab :ripple="false" disable no-caps name="all_company_cards" label="All company cards" />
+        </q-tabs>
 
-      <q-tab-panels class="col-12" v-model="tab" animated no-padding no-margin>
-        <q-tab-panel name="my_debit_cards" no-padding no-margin>
-          <q-card class="my-card">
-            <q-card-section>
+        <q-tab-panels class="col-12" v-model="tab" animated no-padding no-margin>
+          <q-tab-panel name="my_debit_cards" no-padding no-margin>
+            <q-card class="my-card">
+              <q-card-section>
 
-              <div class="row">
-                <div class="col-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                  <Cards />
-                  <CardOptions />
-                </div>
-                <div class="col-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                  <div>
-                    <CardDetails />
+                <div class="row">
+                  <div class="col-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <Cards />
+                    <CardOptions />
                   </div>
-                  <div>
-                    <RecentTransactions />
+                  <div class="col-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div>
+                      <CardDetails />
+                    </div>
+                    <div>
+                      <RecentTransactions />
+                    </div>
                   </div>
                 </div>
-              </div>
 
-            </q-card-section>
-          </q-card>
-        </q-tab-panel>
-      </q-tab-panels>
-    </div>
+              </q-card-section>
+            </q-card>
+          </q-tab-panel>
+        </q-tab-panels>
+      </div>
+    </template>
+
+    <template v-if="isPhone">
+        <Cards />
+        <div class="card">
+          <CardOptions />
+          <CardDetails />
+          <RecentTransactions />
+        </div>
+    </template>
+
+    <PhoneMenu v-if="isPhone" style="position: fixed; bottom: 0; width: 100%;" />
+
+    <!-- ----------------------- -->
 
     <q-dialog v-model="showAddCardDialog">
       <q-card style="min-width: 350px;">
@@ -93,8 +131,6 @@
           <q-btn flat label="Add" color="primary" @click="submit" />
         </q-card-actions>
 
-        
-
       </q-card>
     </q-dialog>
 
@@ -107,7 +143,8 @@ import Cards from '../components/Cards.vue';
 import CardOptions  from '../components/CardOptions.vue';
 import CardDetails from '../components/CardDetails.vue';
 import RecentTransactions from '../components/RecentTransactions.vue';
-// import { useQuasar } from 'quasar'
+import PhoneMenu from '../components/PhoneMenu.vue';
+import { useQuasar } from 'quasar'
 
 export default defineComponent({
   name: 'PageIndex',
@@ -116,12 +153,9 @@ export default defineComponent({
     CardOptions,
     CardDetails,
     RecentTransactions,
+    PhoneMenu,
   },
   setup() {
-    // const $q = useQuasar() // $q.platform.is.mobile
-    // console.log('## IS DESKTOP: ', $q.platform.is.desktop);
-    // console.log('## IS MOBILE: ', $q.platform.is.mobile);
-
     const defaultCard = ref({
       name: '',
       cardNumber: null,
@@ -134,6 +168,12 @@ export default defineComponent({
       showAddCardDialog: ref(false),
       tab: 'my_debit_cards',
       card: defaultCard,
+    }
+  },
+  computed: {
+    isPhone () {
+      const $q = useQuasar()
+      return ($q.platform.is.mobile !== undefined && $q.platform.is.mobile)
     }
   },
   watch: {
@@ -187,3 +227,18 @@ export default defineComponent({
   }
 });
 </script>
+
+<style lang="scss" scoped>
+  .phone-margin {
+    margin: 0px;
+  }
+
+  .web-margin {
+    margin: 40px;
+  }
+
+  .phone-background {
+    // background-color: #0C365A;
+  }
+
+</style>
